cf378cf0036f9147fdcfbb52a79f95fa57cec268
Coding Bird <Codingbird@yahoo.com>
2021-02-23 19:20:03 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const products = [
+  { name: "Photoshop", price: 90.99 },
+  { name: "Adobe", price: 80.99 },
+  { name: "Illustrator", price: 60.92 },
+  { name: "Adobe Reader", price: 8.92 },
+];
 function App() {
+  const myName = "Imran Khan";
+  const style = {
+    color: "red",
+    textAlign: "center",
+    marginTop: "50px",
+  };
+  // const frndList = ["imran", "sobuj", "sojib"];
+  const lo = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={style}>My Heading {myName}</h1>
+      <h3 style={{ color: "blue", fontSize: "60px", textAlign: "center" }}>
+        Hello
+      </h3>
+      {/* {products.map((product) => (
+        <Product name={product.name} price={product.price}></Product>
+      ))} */}
+      {products
+        .filter((p) => p.price > 50)
+        .map((product) => (
+          <Product name={product.name} price={product.price}></Product>
+        ))}
+      {/* {lo && frndList.map(n => <Person name={n}></Person>)} */}
     </div>
   );
 }
 
+const Product = (props) => {
+  const productStyle = {
+    border: "1px solid gray",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    height: "200px",
+    width: "200px",
+    float: "left",
+    margin: "20px",
+    textAlign: "center",
+  };
+  const { name, price } = props;
+  return (
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h1>US${price}</h1>
+      <button>Buy now</button>
+    </div>
+  );
+};
+
+function Person(props) {
+  const { name, data } = props;
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <h4>La la la {data}</h4>
+    </div>
+  );
+}
 export default App;
